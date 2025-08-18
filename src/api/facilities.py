4a885cdac2220965b9f1a39ef9e4739c9fc354ab
Danya_wsl_pycharm <danila.zenkovitch13@yandex.ru>
2025-08-18 16:54:34 +0300

    Задание №21: API тесты на удобства

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -4,13 +4,12 @@ from fastapi_cache.decorator import cache
 
 from src.api.dependencies import DBDep
 from src.schemas.facilities import FacilityAdd
-from src.tasks.tasks import test_task
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 
 @router.get("", summary="Получить список всех удобств")
-@cache(expire=15)
+# @cache(expire=15)
 async def get_facilities(db: DBDep):
     return await db.facilities.get_all()
 
@@ -22,7 +21,5 @@ async def create_facility(
 ):
     facility = await db.facilities.add(facility_data)
     await db.session_commit()
-
-    # test_task.delay()
 
     return {"status": "OK", "data": facility}
