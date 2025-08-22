b663d71e5cfcf9a237df74f9e5edc22bb1dfb930
Danya_wsl_pycharm <danila.zenkovitch13@yandex.ru>
2025-08-22 17:35:59 +0300

    Задание №24: Параметризованный тест на бронирования

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,4 +1,9 @@
 import pytest
+from sqlalchemy import delete
+
+from src.models import BookingsModel
+
+bookings_count = 0
 
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
@@ -33,3 +38,45 @@ async def test_create_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert "data" in res
+
+
+@pytest.fixture(scope="function")
+async def delete_all_bookings(check_test_mode, db):
+    delete_stmt = delete(BookingsModel).returning(BookingsModel)
+    res = await db.session.execute(delete_stmt)
+    await db.session_commit()
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code, my_bookings", [
+    (1, "2024-08-01", "2024-08-10", 200, 1),
+    (1, "2024-08-11", "2024-08-14", 200, 2),
+    (1, "2024-08-15", "2024-08-20", 200, 3),
+])
+async def test_add_and_get_my_bookings(
+        delete_all_bookings,
+        authenticated_ac,
+        room_id,
+        date_from,
+        date_to,
+        status_code,
+        my_bookings,
+
+):
+    response_add_booking = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response_add_booking.status_code == status_code
+
+    response_my_bookings = await authenticated_ac.get(
+        "/bookings/me",
+    )
+    assert response_my_bookings.status_code == 200
+    global bookings_count
+    if response_my_bookings.status_code == 200:
+        bookings_count += 1
+    assert bookings_count == my_bookings
